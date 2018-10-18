deabf6c982baec116324ac89a83a10fa99367f4c
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2018-10-18 21:33:17 +0200

    First version of graphic interface

--- View/src/sample/Controller.java
+++ View/src/sample/Controller.java
@@ -1,4 +1,36 @@
 package sample;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+import javafx.event.ActionEvent;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
 public class Controller {
+    public TextArea t1, t2;
+    public Button button, fileChooser;
+
+    private String file;
+
+    public Controller() {
+
+    }
+
+    public void pressButton(ActionEvent event) {
+        t2.setText(t1.getText());
+    }
+
+    public void pressChooseFile(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+        File file = fileChooser.showOpenDialog(null);
+        System.out.println(file);
+    }
+
+    public String getFile() {
+        return file;
+    }
 }
