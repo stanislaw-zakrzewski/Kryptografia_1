0a2f6b9815c3b02e8e7dcd59f4818ae2c0328a58
Wiktor <wiktor.muraszko@wp.pl>
2018-11-02 22:40:52 +0100

    Key class

--- Logic/src/main/java/keys/Key.java
+++ Logic/src/main/java/keys/Key.java
@@ -1,9 +1,36 @@
 package keys;
 
-public class Key {
-    private byte[] ki;
+import data.Tables;
+import methods.Conversions;
+import methods.Permutacjon;
 
-    public byte[] getKi() {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Key {
+    private List<Byte> ki;
+    public Key(String ki){
+        List<List<Byte>> pom;
+        pom = Conversions.stringTo64Byte(ki);
+        this.ki = pom.get(0);
+        //System.out.println(this.ki.size());
+    }
+    public List<Byte> keyForPermutationI(int permutationNumber){
+        List<Byte> keyAfterPC1 = Permutacjon.PC1Perm(ki);
+        ArrayList<Byte> keyFirstHalf = new ArrayList(keyAfterPC1.subList(0,28));
+        ArrayList<Byte> keySecendHalf = new ArrayList<>(keyAfterPC1.subList(28,56));
+        for(int i = 0; i < Tables.R[permutationNumber];i++){
+            keyFirstHalf.remove(0);
+            keyFirstHalf.add(new Byte((byte)0));
+            keySecendHalf.remove(0);
+            keySecendHalf.add(new Byte((byte)0));
+        }
+        keyFirstHalf.addAll(keySecendHalf);
+        //System.out.println(keyFirstHalf.size());
+        return Permutacjon.PC2Perm(keyFirstHalf);
+    }
+
+    public List<Byte> getKi() {
         return ki;
     }
 
