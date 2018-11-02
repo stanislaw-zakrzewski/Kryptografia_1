c936611644575d78e23fce1846473fe0ecc027be
Florke07 <maciek.socha1@gmail.com>
2018-11-02 20:17:59 +0100

    perm

--- Logic/src/main/java/Main.java
+++ Logic/src/main/java/Main.java
@@ -1,7 +1,20 @@
-import data.Tables;
+
+import methods.Conversions;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Helloo");
+        String dupa = "dupadupa2";
+        List<List<Byte>> b = Conversions.stringTo64Byte(dupa);
+        b.forEach(I -> {I.forEach(System.out::print); System.out.println();});
+
+        System.out.println(b.get(0).get(1));
+
+        List<Byte> bb = b.get(0);
+
+
+
     }
 }
