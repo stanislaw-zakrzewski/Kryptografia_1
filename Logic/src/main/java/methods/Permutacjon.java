c0765adeaf0fa1bcd54d1b7a8809d891f10ed41c
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2018-11-02 21:42:01 +0100

    Added permutations

--- Logic/src/main/java/methods/Permutacjon.java
+++ Logic/src/main/java/methods/Permutacjon.java
@@ -6,36 +6,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Permutacjon {
-    public static List<Byte> IPPerm (List<Byte> list) {
+    public static List<Byte> IPPerm(List<Byte> list) {
         List<Byte> ret = new ArrayList<>();
-
         for (int i : Tables.IP) {
-            ret.add(list.get(i-1));
+            ret.add(list.get(i - 1));
         }
-
-
         return ret;
     }
 
-    public static List<Byte> EPerm (List<Byte> list) {
+    public static List<Byte> EPerm(List<Byte> list) {
         List<Byte> ret = new ArrayList<>();
-
         for (int i : Tables.E) {
-            ret.add(list.get(i-1));
+            ret.add(list.get(i - 1));
         }
-
-
         return ret;
     }
 
-    public static List<Byte> PPerm (List<Byte> list) {
+    public static List<Byte> PPerm(List<Byte> list) {
         List<Byte> ret = new ArrayList<>();
-
         for (int i : Tables.P) {
-            ret.add(list.get(i-1));
+            ret.add(list.get(i - 1));
         }
+        return ret;
+    }
 
+    public static List<Byte> PC1Perm(List<Byte> list) {
+        List<Byte> ret = new ArrayList<>();
+        for (int i : Tables.PC1) {
+            ret.add(list.get(i - 1));
+        }
+        return ret;
+    }
 
+    public static List<Byte> PC2Perm(List<Byte> list) {
+        List<Byte> ret = new ArrayList<>();
+        for (int i : Tables.PC2) {
+            ret.add(list.get(i - 1));
+        }
+        return ret;
+    }
+
+    public static List<Byte> FPPerm(List<Byte> list) {
+        List<Byte> ret = new ArrayList<>();
+        for (int i : Tables.FP) {
+            ret.add(list.get(i - 1));
+        }
         return ret;
     }
 }
