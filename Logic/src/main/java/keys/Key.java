1e940c89242a2ce61a6848c22d9c8a712874a01b
Wiktor <wiktor.muraszko@wp.pl>
2018-11-04 16:39:26 +0100

    Key class (update1.3) refactoring code removing warnings

--- Logic/src/main/java/keys/Key.java
+++ Logic/src/main/java/keys/Key.java
@@ -12,13 +12,13 @@ public class Key {
     public Key(String ki){
         List<List<Byte>> pom;
         pom = Conversions.stringTo64Byte(ki);
-        //pom = Conversions.encodedMessageTo64Byte(ki);//todo
+        //pom = Conversions.encodedMessageTo64Byte(ki);
         this.ki = pom.get(0);
         //System.out.println(this.ki);
     }
-    public List<Byte> keyForPermutationI(int permutationNumber){
+    /*public List<Byte> keyForPermutationI(int permutationNumber){
         List<Byte> keyAfterPC1 = Permutacjon.PC1Perm(ki);
-        ArrayList<Byte> keyFirstHalf = new ArrayList(keyAfterPC1.subList(0,28));
+        ArrayList<Byte> keyFirstHalf = new ArrayList<>(keyAfterPC1.subList(0,28));
         ArrayList<Byte> keySecendHalf = new ArrayList<>(keyAfterPC1.subList(28,56));
         for(int i = 0; i < Tables.R[permutationNumber];i++){
             keyFirstHalf.remove(0);
@@ -31,7 +31,7 @@ public class Key {
         keyFirstHalf.addAll(keySecendHalf);
         //System.out.println(keyFirstHalf.size());
         return Permutacjon.PC2Perm(keyFirstHalf);
-    }
+    }*/
     public ArrayList<List<Byte>> keyTableForEveryRound(){
         ArrayList<List<Byte>> resoult = new ArrayList<>();
         List<Byte> keyAfterPC1 = Permutacjon.PC1Perm(ki);
@@ -39,11 +39,11 @@ public class Key {
         ArrayList<Byte> keySecendHalf;
         for(int permutationNumber = 0; permutationNumber < 16 ; permutationNumber++){
             //System.out.println(keyAfterPC1.size());
-            keyFirstHalf = new ArrayList(keyAfterPC1.subList(0,28));
+            keyFirstHalf = new ArrayList<>(keyAfterPC1.subList(0,28));
             keySecendHalf = new ArrayList<>(keyAfterPC1.subList(28,56));
             for(int i = 0; i < Tables.R[permutationNumber];i++){
                 keyFirstHalf.remove(0);
-                keyFirstHalf.add(new Byte((byte) 0));
+                keyFirstHalf.add((byte) 0);
                 keySecendHalf.remove(0);
                 keySecendHalf.add((byte) 0);
             }
@@ -56,14 +56,6 @@ public class Key {
 
         }
         return resoult;
-    }
-
-    public List<Byte> getKi() {
-        return ki;
-    }
-
-    public void genKi() {
-
     }
 
 
