5083fe25833d5f381e37d0ce4c6173eb3c53d3a6
Wiktor <wiktor.muraszko@wp.pl>
2018-11-04 15:36:14 +0100

    Key class (update)

--- Logic/src/main/java/keys/Key.java
+++ Logic/src/main/java/keys/Key.java
@@ -14,7 +14,7 @@ public class Key {
         //pom = Conversions.stringTo64Byte(ki);
         pom = Conversions.encodedMessageTo64Byte(ki);//todo
         this.ki = pom.get(0);
-        //System.out.println(this.ki.size());
+        //System.out.println(this.ki);
     }
     public List<Byte> keyForPermutationI(int permutationNumber){
         List<Byte> keyAfterPC1 = Permutacjon.PC1Perm(ki);
@@ -22,13 +22,39 @@ public class Key {
         ArrayList<Byte> keySecendHalf = new ArrayList<>(keyAfterPC1.subList(28,56));
         for(int i = 0; i < Tables.R[permutationNumber];i++){
             keyFirstHalf.remove(0);
-            keyFirstHalf.add(new Byte((byte)0));
+            keyFirstHalf.add((byte) 0);
             keySecendHalf.remove(0);
-            keySecendHalf.add(new Byte((byte)0));
+            keySecendHalf.add((byte)0);
         }
+        //System.out.println(keyAfterPC1);
+        //System.out.println(keyFirstHalf + " + " + keySecendHalf);
         keyFirstHalf.addAll(keySecendHalf);
         //System.out.println(keyFirstHalf.size());
         return Permutacjon.PC2Perm(keyFirstHalf);
+    }
+    public ArrayList<List<Byte>> keyTableForEveryRound(){
+        ArrayList<List<Byte>> resoult = new ArrayList<>();
+        List<Byte> keyAfterPC1 = Permutacjon.PC1Perm(ki);
+        ArrayList<Byte> keyFirstHalf;
+        ArrayList<Byte> keySecendHalf;
+        for(int permutationNumber = 0; permutationNumber < 16 ; permutationNumber++){
+            keyFirstHalf = new ArrayList(keyAfterPC1.subList(0,28));
+            keySecendHalf = new ArrayList<>(keyAfterPC1.subList(28,56));
+            for(int i = 0; i < Tables.R[permutationNumber];i++){
+                keyFirstHalf.remove(0);
+                keyFirstHalf.add((byte) 0);
+                keySecendHalf.remove(0);
+                keySecendHalf.add((byte) 0);
+            }
+            //System.out.println(keyAfterPC1);
+            //System.out.println(keyFirstHalf + " + " + keySecendHalf);
+            keyFirstHalf.addAll(keySecendHalf);
+            //System.out.println(keyFirstHalf.size());
+            resoult.add(Permutacjon.PC2Perm(keyFirstHalf));
+            keyAfterPC1 = Permutacjon.PC2Perm(keyFirstHalf);
+
+        }
+        return resoult;
     }
 
     public List<Byte> getKi() {
